588f734d98889787155e333e9fc1133847a1c3a2
nw-com <nwcom.eason@gmail.com>
2025-12-01 17:03:27 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyAmll4i3RPI_j-J7qWPkZIBYPoWEFqL9os",
+  authDomain: "nw-finance-2026.firebaseapp.com",
+  projectId: "nw-finance-2026",
+  storageBucket: "nw-finance-2026.firebasestorage.app",
+  messagingSenderId: "234539651970",
+  appId: "1:234539651970:web:1d14d3778831109634359b",
+  measurementId: "G-VV8QRLHKC9"
+});
+window.__force_demo_mode = false;
